6a283fa4c2c4d53d86da27e4744df421a2e7a405
Tatti Vitorino <tatti@tattivitorino.com>
2020-02-21 17:23:39 -0300

    Init Native Firebase APP IOs/Android

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
+import firebase from '@react-native-firebase/app';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
+  componentDidMount() {
+    console.log(firebase.app().name)
+  }
   render() {
     return (
       <View style={styles.container}>
